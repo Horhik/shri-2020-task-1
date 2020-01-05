0ee1f7a36a24dda9491284046bf58aacfc3ff1dc
horhi <horh1khcj@gmail.com>
2020-01-05 22:40:16 +0300

    add layout.scss without js

--- stub/build/script.js
+++ stub/build/script.js
@@ -1,39 +1,89 @@
-const  theme ={
-color:{
-    projectDefault{},
-    projectBrand{},
-    projectInverse{},
-    projectWarning{},
-}
-}
-
-color 	project-default, project-brand, project-inverse, project-warning 	Цвета всех элементов интерфейса
-space 	default 	Размеры отступов между элементами интерфейса
-size 	default 	Размеры типографических величин
-
-В значениях модификатора color должны содержаться переменные для основных состояний фонов блоков и типографики.
-    .theme_color_project-default — цветовая схема со светлым фоном и яркими акцентными контролами
-
-Значения фонов блоков:
-    - дефолтный #FFFFFF;
-- акцентный #ffcc00;
-- бордерный #000000.
-
-    Значения состояний текста:
-    - основной: #000000;
-- второстепенный: rgba(0, 0, 0, 0.6);
-- дополнительный: rgba(0, 0, 0, 0.3),
-    - ссылочный: #0077ff.
-
-    .theme_color_project-brand — цветовая схема с ярким фоном и светлыми акцентными контролами
-
-Значения фонов блоков:
-    - дефолтный: #0077ff;
-- акцентный: #FFFFFF;
-- бордерный: #FFFFFF.
-
-    Значения состояний текста:
-    - основной: #FFFFFF;
-- второстепенный: rgba(255, 255, 255, 0.6);
-- дополнительный: rgba(255, 255, 255, 0.3),
-    - ссылочный: #FFFFFF.
+const theme = {
+  color: {
+    projectDefault: {
+      background: {
+        default: "#FFFFFF",
+        accent: "#ffcc00",
+        border: "#000000",
+      },
+      text: {
+        primary: "#000000",
+        secondary: "rgba(255, 255, 255, 0.6)",
+        additional: "rgba(255, 255, 255, 0.3)",
+        link: "#0077ff"
+      }
+    },
+    projectBrand: {
+      background: {
+        default: "#0077ff",
+        accent: "#FFFFFF",
+        border: "#FFFFFF",
+      },
+      text: {
+        primary: "#FFFFFF",
+        secondary: "rgba(255, 255, 255, 0.6)",
+        additional: "rgba(255, 255, 255, 0.3)",
+        link: "#FFFFFF"
+      }
+    },
+    projectInverse: {
+      background: {
+        default: "#000000",
+        accent: "#FFFFFF",
+        border: "#FFFFFF",
+      },
+      text: {
+        primary: "#FFFFFF",
+        secondary: "rgba(255, 255, 255, 0.6)",
+        additional: "rgba(255, 255, 255, 0.3)",
+        link: "#0077ff"
+      }
+    },
+    projectWarning: {
+      background: {
+        default: "#FF3333",
+        accent: "#FFFFFF",
+        border: "#FF3333",
+      },
+      text: {
+        primary: "#FFFFFF",
+        secondary: "rgba(255, 255, 255, 0.6)",
+        additional: "rgba(255, 255, 255, 0.3)",
+        link: "#FFFFFF"
+      }
+    }
+  },
+  space: {
+    default: {
+      xxxs: 3,
+      xxs:6,
+      xs: 9,
+      s: 15,
+      m: 18,
+      l: 24,
+      xl: 27,
+      xxl: 36,
+      xxxl: 45,
+      xxxxl: 51,
+      xxxxxl: 72,
+    }
+  },
+  size: {
+    default: {
+      height: {
+        s: 8,
+        m: 10,
+        l: 11,
+        xl: 13,
+        xxl: 16,
+      },
+      lineHeight: {
+        s: 21,
+        m:25,
+        l: 28,
+        xl: 34,
+        xxl: 38,
+      },
+    }
+  },
+};
